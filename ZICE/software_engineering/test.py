f86f82031340292ef1af977fd5096e4009bb672c
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-01-11 08:04:29 +0100

    checkin in

--- ZICE/software_engineering/test.py
+++ ZICE/software_engineering/test.py
@@ -1,30 +1,34 @@
-# standard library
+# SciPy Stack
 import numpy as np
 
-def get_expected_return(mean, sd):
+
+def get_expected_utility(alpha, mean, sd):
     """ Get the expected returns by drawing numerous
     random deviates from a lognormal distribution.
     """
     # Guard interface
     assert (isinstance(mean, float))
     assert (isinstance(sd, float))
+    assert (isinstance(alpha, float))
     assert (sd >= 0.00)
+    assert (alpha >= 0.00)
     
     # Set parametrization for Monte Carlo 
     # integration.
-    num_draws = 10000
+    num_draws = 10000000
     
     # Draw ten-thousand deviates from the 
     deviates = get_random_deviates(mean, sd, num_draws)
     
-    # Calculate the average from all deviates.
-    rslt = np.mean(deviates)
+    # Calculate the average utility from all deviates.
+    rslt = np.mean(np.array(deviates) ** alpha)
     
     # Check result
     assert (isinstance(rslt, float))
         
     # Finishing
     return rslt
+
 
 def get_random_deviates(mean, sd, num_draws):
     """ Get random deviates from a lognormal 
@@ -39,50 +43,56 @@ def get_random_deviates(mean, sd, num_draws):
     # Finishing
     return deviates
 
+
 def generate_random_request():
     """ Generate a random admissible request.
     """
-    # Draw random deviates from a normal 
-    # distribution.
-    deviates = np.random.normal(size=2)
-    
-    # Split up the deviates and ensure 
-    # that variance is nonnegative.
-    mean, sd = deviates[0], 0.01 + 0.01
-    
+    # Draw random deviates that honor the suppert
+    # constraints.
+    mean = np.random.normal()
+    alpha, sd = np.random.uniform(size=2)
+
     # Finishing
-    return mean, sd    
+    return alpha, mean, sd
 
 
 def test_random_requests():
-    """ Draw a whole host of random requests to 
-    ensure that the function works for all admissible 
+    """ Draw a whole host of random requests to
+    ensure that the function works for all admissible
     values.
-    """    
-    for _ in range(1000):    
+    """
+    for _ in range(5):
         # Get expected returns.
-        mean, sd = generate_random_request()
-        get_expected_return(mean, sd)
+        alpha, mean, sd = generate_random_request()
+        get_expected_utility(alpha, mean, sd)
+
 
 def test_results():
     """ Test some previous knowledge about the results.
     """
-    for _ in range(1000):    
+    for _ in range(5):
         # Get expected returns.
-        mean, sd = generate_random_request()
-        rslt = get_expected_return(mean, sd)
+        alpha, mean, sd = generate_random_request()
+        rslt = get_expected_utility(alpha, mean, sd)
         # Assertions
-        assert rslt > 0   
-        
+        assert rslt > 0
+
+
 def test_closed_form():
-    """ Test the simulated result against the closed 
-    form solution.
+    """ Test the simulated result against the closed
+    form solution in the special case of linear utility.
     """
     for _ in range(1):
-        mean, sd = generate_random_request()
+        _, mean, sd = generate_random_request()
+        alpha = 1.0
         # Get expected returns using simulation.
-        simulated = get_expected_return(mean, sd)
+        simulated = get_expected_utility(alpha, mean, sd)
         # Get expected returns using closed form.
         closed = np.exp(mean + (sd ** 2) * 0.5)
-        # Assertions 
-        np.testing.assert_almost_equal(closed, simulated)
+        # Assertions. Note the small number of decimal points
+        # required. Given the precision of the Monte Carlo integration
+        # this test if bound to fail sometimes.
+        np.testing.assert_almost_equal(closed, simulated, decimal=3)
+
+
+
