9847d8a820d0911cdd459a495c64c031f91da2f2
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-02-01 20:50:30 +0100

    edits

--- ZICE/scientific_python/example/run.py
+++ ZICE/scientific_python/example/run.py
@@ -19,21 +19,28 @@ betas = np.array([0.22, 0.30, -0.1]).T
 Y, X = random_sample(betas, num_agents)
 
 # Perform a basic OLS regression and print out summary information.
-rslt = sm.OLS(Y, sm.add_constant(X)).fit()
+X = sm.add_constant(X)
+rslt = sm.OLS(Y, X).fit()
 print(rslt.summary())
 
 # Show interactive plot.
+# Initialize canvas
 ax = plt.figure(figsize=(12, 8)).add_subplot(111, axisbg='white')
 
-# Plot synthetic sample
-ax.plot(np.dot(X, betas), Y, 'o')
+# Plot actual and fitted values.
+ax.plot(np.dot(X, rslt.params), Y, 'o', label='True')
+ax.plot(np.dot(X, rslt.params), rslt.fittedvalues, 'r--.', label="Predicted")
 
 # Set axis labels and ranges
-ax.set_xlabel(r'$X\beta$', fontsize=20)
+ax.set_xlabel(r'$X\hat{\beta}$', fontsize=20)
 ax.set_ylabel(r'$Y$', fontsize=20)
 
 # Remove first element on y-axis
 ax.yaxis.get_major_ticks()[0].set_visible(False)
+
+# Add legend
+plt.legend(loc='upper center', bbox_to_anchor=(0.50, -0.10),
+    fancybox=False, frameon=False, shadow=False, ncol=2, fontsize=20)
 
 # Add title
 plt.suptitle('Synthetic Sample', fontsize=20)
