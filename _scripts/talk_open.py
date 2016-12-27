b8c0f0ca24478c473b2034de9c219aadaf0eea3b
peisenha <eisenhauer@policy-lab.org>
2016-12-27 17:48:46 +0100

    edits

--- _scripts/talk_open.py
+++ _scripts/talk_open.py
@@ -5,8 +5,5 @@
 # standard library
 import os
 
-# Ensure most recent version available
-os.system('talk_build')
-
 # Create and serve the slides.
 os.system('ipython nbconvert --to slides talk.ipynb --post serve')
--- _scripts/talk_publish.py
+++ _scripts/talk_publish.py
@@ -5,9 +5,6 @@
 # standard library
 import os
 
-# Build all material.
-os.system('talk_build')
-
 # Publish most recent material of current directory online.
 os.system("git commit -m'committing' -- .")
 os.system("git push")
