0a84b0787b169b15da11f447edab9c43907fac30
peisenha <eisenhauer@policy-lab.org>
2017-01-07 18:02:18 +0100

    edits

--- intro_structural_estimation/example/auxiliary.py
+++ intro_structural_estimation/example/auxiliary.py
@@ -1,3 +1,4 @@
+import statsmodels.api as sm
 from scipy.stats import norm
 import numpy as np
 
@@ -21,11 +22,12 @@ def simulate_sample(theta, num_obs, seed=123):
 
 
 def criterion_ml(theta, data_obs, type_, num_sim=None):
-    """ This function allows to evaluate the criterion for maximum likelihood estimation.
+    """ This function evaluates the criterion for traditional and simulation-based maximum
+    likelihood estimation.
     """
-    if type_ == 'classical':
+    if type_ == 'traditional':
         probs = norm.cdf(theta)
-    elif type_ == 'simulated':
+    elif type_ == 'AR-simulator':
         data_sim, _ = simulate_sample(theta, num_sim)
         probs = np.mean(data_sim)
     else:
@@ -37,7 +39,7 @@ def criterion_ml(theta, data_obs, type_, num_sim=None):
 
 
 def get_smoothed_probabilities(u, lambda_):
-    """ This function simply applys the smoothing function to the latent utilites.
+    """ This function simply applies the Logit smoothing function to the latent utilities.
     """
     num_sim = u.shape[0]
 
@@ -51,14 +53,15 @@ def get_smoothed_probabilities(u, lambda_):
 
 
 def criterion_mm(theta, data_obs, type_, num_sim=None, lambda_=1.0):
-    """ This function allows to evaluate the criterion function for the method of moments.
+    """ This function evaluates the criterion for traditional and simulation-based method of
+    moments estimation.
     """
-    if type_ == 'classical':
+    if type_ == 'traditional':
         stat = norm.cdf(theta)[0]
-    elif type_ == 'simulated':
+    elif type_ == 'AR-simulator':
         data_sim, _ = simulate_sample(theta, num_sim)
         stat = np.mean(data_sim)
-    elif type_ == 'smoothed':
+    elif type_ == 'smoothed-AR-simulator':
         _, u = simulate_sample(theta, num_sim)
         stat = np.mean(get_smoothed_probabilities(u, lambda_), axis=0)[1]
     else:
@@ -67,3 +70,18 @@ def criterion_mm(theta, data_obs, type_, num_sim=None, lambda_=1.0):
     rslt = (np.mean(data_obs) - stat) ** 2
 
     return rslt
+
+
+def illustration_ii(theta, num_sim):
+    """ This function serves to illustrate the reasoning behind indirect inference estimation.
+    """
+    data_sim, _ = simulate_sample(theta, num_sim)
+    rslt = sm.OLS(data_sim, np.tile(1, len(data_sim))).fit().params[0]
+
+    return rslt
+
+
+def logistic_function(x, lambda_):
+    """ Logistic function that allows to smooth the simulated probabilites.
+    """
+    return np.exp(x / lambda_) / (1 + np.exp(x / lambda_))
--- intro_structural_estimation/example/illustrations.py
+++ intro_structural_estimation/example/illustrations.py
@@ -1,36 +1,54 @@
 """ This module allows to illustrate alternative estimation techniques for structural estimation.
 All is based on a simple binary Probit model.
 """
-
+import statsmodels.api as sm
 import numpy as np
 
+from auxiliary import logistic_function
 from auxiliary import simulate_sample
-from auxiliary import criterion_ml
+from auxiliary import illustration_ii
 from auxiliary import criterion_mm
+from auxiliary import criterion_ml
 
+from graphs import graphs_indirect_inference
 from graphs import graphs_smoothing_functions
 from graphs import graphs_maximum_likelihood
 from graphs import graphs_method_moments
+from graphs import graphs_logit_function
 
-# First of all we need to simulate a sample of observed data.
+# First of all we need to simulate a sample of observed data and also create a grid along which
+# we will evaluate the alternative criterion functions.
+grid = np.array(np.linspace(-0.5, 0.5, 100), ndmin=2)
 data_obs, _ = simulate_sample(0.0, 100000, 465)
 
 # This loop shows how the number of simulations smoothes out the likelihood function.
 for num_sim in [100, 10000]:
     grid = np.array(np.linspace(-0.5, 0.5, 100), ndmin=2)
-    classical_values = np.apply_along_axis(criterion_ml, 0, grid, data_obs, 'classical')
-    simulated_values = np.apply_along_axis(criterion_ml, 0, grid, data_obs, 'simulated', num_sim)
+    classical_values = np.apply_along_axis(criterion_ml, 0, grid, data_obs, 'traditional')
+    simulated_values = np.apply_along_axis(criterion_ml, 0, grid, data_obs, 'AR-simulator', num_sim)
     graphs_maximum_likelihood(grid[0], classical_values, simulated_values, num_sim)
 
-# The same is true for the method of moments.
-grid = np.array(np.linspace(-0.5, 0.5, 100), ndmin=2)
-classical_values = np.apply_along_axis(criterion_mm, 0, grid, data_obs, 'classical')
-simulated_values = np.apply_along_axis(criterion_mm, 0, grid, data_obs, 'simulated', 100, 0.00)
+# Now let me also create a graph for the simulated method of moments.
+classical_values = np.apply_along_axis(criterion_mm, 0, grid, data_obs, 'traditional')
+simulated_values = np.apply_along_axis(criterion_mm, 0, grid, data_obs, 'AR-simulator', 100, 0.00)
 graphs_method_moments(grid[0], classical_values, simulated_values, 100)
 
 # Now we also check the it for the smoothing function.
-grid = np.array(np.linspace(-0.5, 0.5, 100), ndmin=2)
-classical_values = np.apply_along_axis(criterion_mm, 0, grid, data_obs, 'classical')
-smoothing_1 = np.apply_along_axis(criterion_mm, 0, grid, data_obs, 'simulated', 100, 0.00)
-smoothing_2 = np.apply_along_axis(criterion_mm, 0, grid, data_obs, 'smoothed', 100, 0.50)
+classical_values = np.apply_along_axis(criterion_mm, 0, grid, data_obs, 'traditional')
+smoothing_1 = np.apply_along_axis(criterion_mm, 0, grid, data_obs, 'AR-simulator', 100, 0.00)
+smoothing_2 = np.apply_along_axis(criterion_mm, 0, grid, data_obs, 'smoothed-AR-simulator', 100, 0.50)
 graphs_smoothing_functions(grid[0], classical_values, smoothing_1, smoothing_2)
+
+# I want to fit a linear probability model to illustrate the workings of indirect inference.
+grid = np.array(np.linspace(-0.5, 0.5, 100), ndmin=2)
+observed_values = np.tile(sm.OLS(data_obs, np.tile(1, len(data_obs))).fit().params[0], 100)
+simulated_values = np.apply_along_axis(illustration_ii, 0, grid, 1000)
+graphs_indirect_inference(grid[0], observed_values, simulated_values)
+
+# For illustrative purposes, I also plot the logistic function. Note, that the grid needs to be
+# redefined here.
+grid = np.array(np.linspace(-3, 3, 100), ndmin=2)
+values_0_10 = np.apply_along_axis(logistic_function, 0, grid, 0.10)
+values_0_25 = np.apply_along_axis(logistic_function, 0, grid, 0.25)
+values_0_75 = np.apply_along_axis(logistic_function, 0, grid, 0.75)
+graphs_logit_function(grid[0], values_0_10[0], values_0_25[0], values_0_75[0])
